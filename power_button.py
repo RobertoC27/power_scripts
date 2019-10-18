a5312b05ba8854e9534a2adad6365ae3ca041794
Roberto <chiroy_134@hotmail.com>
2019-10-18 09:07:43 -0600

    poweroff if button released within 10 secs

--- power_button.py
+++ power_button.py
@@ -1,20 +1,30 @@
 from gpiozero import  Button, LED
 from signal import pause
 import subprocess
+from datetime import datetime
 
 power_button = Button(3, hold_time=5)
+allow_shutdown = False
+pressed_time = datetime.now()
 
 def shutdown():
-	subprocess.call(["sudo", "shutdown", "now"])
+	time_delta = (datetime.now() - pressed_time).total_seconds()
+	if(allow_shutdown and time_delta > 10):
+		allow_shutdown = False
+	elif(allow_shutdown and time_delta <= 10):
+		subprocess.call(["sudo", "shutdown", "now"])
+
+def change_state():
+	allow_shutdown = True
+	pressed_time = datetime.now()
 
 red_led =  LED(17)
-#green_led = LED(18)
 
 red_led.source = power_button
-#green_led.source = power_button
 
+power_button.when_held = change_state
+power_button.when_released = shutdown
 
-power_button.when_held = shutdown
 pause()
 
 
